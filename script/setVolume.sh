3225cae6804f8b3a3d17b1ed37b624118fd0888a
sickleaf <5824061+sickleaf@users.noreply.github.com>
2018-04-08 23:35:13 +0900

    [fix] setVolume.sh; auto detect wired/bluetooth,removed output argument. (issue #4 still remains)

--- script/setVolume.sh
+++ script/setVolume.sh
@@ -10,21 +10,30 @@
 
 if [ $# -lt 2 ]; then
 	echo "<<usage>>"
-	echo "case 1[absolute,wired] : $0 volLevel 0 "
-	echo "ex. set absolute volume(PCM) 90% ->  $0 90 0 "
+	echo "1) specify absolute volume level"
+	echo "ex. set 90% ->  $0 90 "
 	echo ""
-	echo "case 2[relative,wired] : $0 stepNum +/-  "
-	echo "ex. decrease volume(PCM) 4% ) -> $0 4 - "
-	echo ""
-	echo "case 3[absolute,bluetooth] : $0 volLevel 0 0"
-	echo "ex. set absolute volume(bluealsa) 70% ->  $0 90 0 0"
-	echo ""
-	echo "case 4[relative,bluetooth] : $0 stepNum +/-  "
-	echo "ex. increase volume(bluealsa) 4% ) -> $0 4 + 0"
+	echo "2) specify relative volume level"
+	echo "ex 1. increase 5% ->  $0 5 + "
+	echo "ex 2. decrease 4% ->  $0 4 - "
+#	echo ""
+#	echo "case 1[absolute,wired] : $0 volLevel 0 "
+#	echo "ex. set absolute volume(PCM) 90% ->  $0 90 0 "
+#	echo ""
+#	echo "case 2[relative,wired] : $0 stepNum +/-  "
+#	echo "ex. decrease volume(PCM) 4% ) -> $0 4 - "
+#	echo ""
+#	echo "case 3[absolute,bluetooth] : $0 volLevel 0 0"
+#	echo "ex. set absolute volume(bluealsa) 70% ->  $0 90 0 0"
+#	echo ""
+#	echo "case 4[relative,bluetooth] : $0 stepNum +/-  "
+#	echo "ex. increase volume(bluealsa) 4% ) -> $0 4 + 0"
 	exit 1
 fi
 
-if [ $# -eq 3 ]; then
+bluetoothFlg=$(hcitool con | grep ACL | wc -l)
+
+if [ ${bluetoothFlg} -eq 1 ]; then
 	target=$(amixer -D bluealsa | grep " - A2DP" | head -1 | cut -d "'" -f 2)
 	daemonOption=" -D  bluealsa "
 	unit="%"
@@ -36,14 +45,10 @@ fi
 
 #echo "daemonOption="${daemonOption}",target:\"${target}\""
 
-
 if [ "$2" = '+' ]; then 
-#	echo "amixer ${daemonOption} sset \"${target}\" `expr $1`${unit}+"
 	sh -c "amixer ${daemonOption} sset \"${target}\" `expr $1`${unit}+"
 elif [ "$2" = '-' ]; then 
-#	echo "amixer ${daemonOption} sset \"${target}\" `expr $1`${unit}-"
 	sh -c "amixer ${daemonOption} sset \"${target}\" `expr $1`${unit}-"
 else
-#	echo "amixer ${daemonOption} sset \"${target}\" `expr $1`%"
 	sh -c "amixer ${daemonOption} sset \"${target}\" `expr $1`%"
 fi
