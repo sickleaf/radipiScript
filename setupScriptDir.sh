0356f6ba60418cd4da00a9d02ca6ae95328591a1
radipiv2 <test@example.com>
2021-05-09 20:19:03 +0900

    [rev] (1)removed browserdir (2)copy only scriptdir

--- setupScriptDir.sh
+++ setupScriptDir.sh
@@ -9,16 +9,11 @@ BASESCRIPTNAME=script;
 
 # script name in local environment 
 # /home/${RADIPIUSER}/${SCRIPTNAME}
-# /home/${RADIPIUSER}/${SCRIPTNAME}/${BROWSERSCRIPTNAME}
 # /home/${RADIPIUSER}/${SCRIPTNAME}/${CONFIGNAME}
+
 SCRIPTNAME=Script;
-BROWSERSCRIPTNAME=browserScript;
 CONFIGNAME=config;
-
-
-# USER
-
-RADIPIUSER=radipi;
+RADIPIUSER=$(id -u -n);
 
 ################################################
 
@@ -26,64 +21,24 @@ RADIPIUSER=radipi;
 # BASEDIR=		/home/Repository/radipiScript/${BASESCRIPTNAME}
 
 # SCRIPTDIR=		/home/${RADIPIUSER}/${SCRIPTNAME}
-# BROWSERSCRIPTDIR=	/home/${RADIPIUSER}/${SCRIPTNAME}/${BROWSERSCRIPTNAME}
 
 BASEDIR=$(dirname $0)/${BASESCRIPTNAME};
-
 SCRIPTDIR=/home/${RADIPIUSER}/${SCRIPTNAME}
-BROWSERSCRIPTDIR=${SCRIPTDIR}/${BROWSERSCRIPTNAME}
 
 # PATH
 
 ROOTPATH=$(cd $(dirname $0); pwd);
-BASEDIRFULLPATH=$(cd ${BASEDIR}; pwd);
-
+SCRIPTROOTPATH=${ROOTPATH}/${BASESCRIPTNAME};
 
 echo "######################################";
 echo "<< setupScriptDir.sh>> setup START.";
 echo "######################################";
 
-################################################
-# [mkdir] SCRIPTDIR
-################################################
+echo "[make directory]";
+mkdir -pv ${SCRIPTDIR};
 
-mkdir -p ${SCRIPTDIR};
-if [ $? -eq 0 ]; then
-	echo "[make directory] ${SCRIPTDIR}";
-fi
+find ${SCRIPTROOTPATH} | cut -d/ -f7- | xargs -I@ install -pvD ${SCRIPTROOTPATH}/@ ${SCRIPTDIR}/@
 
-################################################
-# [cp]scripts in BASEDIR into SCRIPTDIR
-################################################
-
-BASESCRIPTLIST=$(ls -ld ${BASEDIRFULLPATH}/*.sh | awk '{print $NF}');
-for EACHSCRIPT in ${BASESCRIPTLIST};
-do
-	FILENAME=$(basename $EACHSCRIPT);
-	cp ${EACHSCRIPT} ${SCRIPTDIR}/${FILENAME};
-	if [ $? -eq 0 ]; then
-		echo "[copy] ${EACHSCRIPT} -> ${SCRIPTDIR}/${FILENAME}";
-	fi
-done;
-
-
-################################################
-# [chmod]add permission(execute) into scripts in SCRIPTDIR
-################################################
-
-chmod 755 ${SCRIPTDIR}/*.sh
-if [ $? -eq 0 ]; then
-	echo "[chmod] ${SCRIPTDIR}/*.sh set permission as 755";
-fi
-
-################################################
-# [cp]copy SCRIPTDIR/config folder
-################################################
-
-cp -pR ${ROOTPATH}/${CONFIGNAME} ${SCRIPTDIR};
-if [ $? -eq 0 ]; then
-	echo "[copy folder] ${ROOTPATH}/${CONFIGNAME} -> ${SCRIPTDIR}/${CONFIGNAME}";
-fi
 
 echo "######################################";
 echo "<< setupScriptDir.sh>> setup COMPLETE.";
