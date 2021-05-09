672ac4115bdfae544998b0fb676627f0fd90e09b
sickleaf <5824061+sickleaf@users.noreply.github.com>
2021-05-09 19:04:15 +0900

    [rev] both stdout and stderr write to flasklog

--- script/startFlask.sh
+++ script/startFlask.sh
@@ -4,5 +4,5 @@ forceFlag=${1:-""}
 
 if [ "$(ps aux | grep flask[P]age)" = "" -o ! "${forceFlag}" = "" ]; then
 	kill -TERM $(ps aux | grep flask[P]age | awk '$0=$2')
-	nohup python3 /home/radipi/Script/webdir/flaskPage.py > /home/radipi/flasklog &
+	nohup python3 /home/radipi/Script/webdir/flaskPage.py > /home/radipi/flasklog 2>&1 &
 fi
