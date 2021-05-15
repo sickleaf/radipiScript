1c6ae9091f9e836eb3d79cb8d924d00d89066004
radipiv2 <test@example.com>
2021-05-15 15:37:32 +0900

    [rev] for bluetooth

--- checkAudio.sh
+++ checkAudio.sh
@@ -7,12 +7,17 @@ bash ${ROOTPATH}/script/setVolume.sh ${volume}
 
 type mpv > /dev/null
 
-
-
 if [ $? -ne 0 ]; then
 	echo 'mpv does not installed. run "apt install mpv -y"'
 else
 	echo "mpv installed."
 fi
 
-find /usr/share/sounds/* -name "*.wav" | grep Side | xargs -n1 mpv 
+bluetoothaddr=$(hcitool con | grep -Eo "[0-9A-F:]{9,}")
+
+mpvOption=""
+
+[ "$(echo $bluetoothaddr)" = "" ] || mpvOption=" --audio-device=alsa/bluealsa"
+
+
+find /usr/share/sounds/* -name "*.wav" | grep Side | xargs -n1 mpv  ${mpvOption}
