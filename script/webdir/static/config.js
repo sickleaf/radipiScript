894198e0be194d93021a1e20257bf8a74bd5b009
radipiv2 <test@example.com>
2021-06-04 00:49:30 +0900

    [rev] (1) added setConfig (2) removed old line

--- script/webdir/static/config.js
+++ script/webdir/static/config.js
@@ -5,9 +5,6 @@ var Radipi = {};
 
 Radipi.Const = function(){};
 
-Radipi.hMax = 24; // hour:00-23
-Radipi.mMax = 60;// min :00-59
-
 Radipi.getAudioInterval=7000;
 
 Radipi.scriptPath = "/home/radipi/Script/";
@@ -23,6 +20,8 @@ Radipi.radikoRandom="/radiko?param=R"
 
 Radipi.playStreaming="/streaming?param="
 Radipi.randomStreaming="/streaming?param=R&setList=/home/radipi/Script/streamingListBase"
+
+Radipi.setConfig="/setConfig?param=bash /home/radipi/Script/setConfig.sh true"
 
 Radipi.radipiDrive="/mntRadio?param=/mnt/radipiDrive/"
 Radipi.mnt1="/mntRadio?param=/mnt/radipiDrive/"
@@ -50,39 +49,3 @@ Radipi.back20sec="/mpvSocket?param= seek -20"
 Radipi.for20sec="/mpvSocket?param= seek 20"
 Radipi.for10p="/mpvSocket?param= seek '\"10\" ,\"relative-percent\"'"
 Radipi.forward="/mpvSocketDelay?param= quit 0"
-
-Radipi.volumeScript = "setVolume.sh";
-Radipi.killScript = "killsound.sh";
-Radipi.radikoScript = "playradiko.sh";
-Radipi.streamingScript= "playStreaming.sh";
-Radipi.playMp3Script = "playLocalfile.sh";
-Radipi.getMp3NameScript = "getMp3Name.sh";
-Radipi.fmScript= "playFM.sh";
-
-Radipi.nowplayingID = "presentID";
-Radipi.nowplayingDirID = "presentDirID";
-
-Radipi.areaText = "areaName";
-Radipi.areaValue = "areaID";
-Radipi.radikoText = "radikoName";
-Radipi.radikoValue = "radikoID";
-Radipi.streamingText = "streamingText";
-Radipi.streamingValue = "streamingValue";
-Radipi.dirText = "dirText";
-Radipi.dirValue = "dirValue";
-Radipi.fmText = "fmName";
-Radipi.fmValue = "fmFreq";
-
-Radipi.timefreePrefix = "time";
-
-Radipi.sleepCommand = "sleep 1 ";
-Radipi.mpvCommand = "mpv --no-video --msg-level=all=info --msg-time  ";
-Radipi.weekdays =  new Array('Sun','Mon','Tue','Wed','Thr','Fri','Sat');
-
-function readConfig(){
-Radipi.radikoListInfo = getFile("config/radikoList.csv");
-Radipi.areaListInfo = getFile("config/areaList.csv");
-Radipi.streamingListInfo = getFile("config/streamingList.csv");
-Radipi.dirListInfo = getFile("config/dirList.csv");
-Radipi.fmListInfo = getFile("config/fmList.csv");
-}
