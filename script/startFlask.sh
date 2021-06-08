6e83247dbc7544d5645dea8912686ac711add379
radipiv2 <test@example.com>
2021-06-09 01:06:13 +0900

    [rev] skip type Enter after nohup command

--- script/startFlask.sh
+++ script/startFlask.sh
@@ -3,6 +3,6 @@
 forceFlag=${1:-""}
 
 if [ "$(ps aux | grep flask[P]age)" = "" -o ! "${forceFlag}" = "" ]; then
-	kill -TERM $(ps aux | grep flask[P]age | awk '$0=$2')
+	kill -TERM $(ps aux | grep flask[P]age | awk '$0=$2') 2>/dev/null
 	nohup python3 /home/radipi/Script/webdir/flaskPage.py > /home/radipi/flasklog &
 fi
